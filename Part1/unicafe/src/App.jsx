9f2d75d7f3b8bbff122e00d670099b9e919484b7
Artoemius <Artoemius@users.noreply.github.com>
2025-07-13 14:45:11 +0200

    Exercises 1.6 - 1.10

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Header = ({text}) => <div><b>{text}</b></div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticsLine = ({text, value}) => {
+ // if (isNaN(value)) {value = ' '}
+  if (text === 'positive') {value = value+' %'}
+  return(<div>{text} {value}</div>)
+}
+
+const Statistics = (props) => {
+  if (props.all===0) {
+    return(<div>No feedback given</div>)
+  }
+  return(
+    <div>
+      <StatisticsLine text='good' value={props.good}/>
+      <StatisticsLine text='neutral' value={props.neutral}/>
+      <StatisticsLine text='bad' value={props.bad}/>
+      <StatisticsLine text='all' value={props.all}/>
+      <StatisticsLine text='average' value={props.average}/>
+      <StatisticsLine text='positive' value={props.positive}/>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState(0)
+
+  const handlePositiveClick = () => {
+    setGood(good+1)
+    setTotal(total+1)
+    setScore(score+1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1)
+    setTotal(total+1)
+  }
+  const handleNegativeClick = () => {
+    setBad(bad+1)
+    setTotal(total+1)
+    setScore(score-1)
+  }
+
+  return (
+    <div>
+      <Header text='give feedback'/>
+      <Button onClick={handlePositiveClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleNegativeClick} text='bad'/>
+      <Header text='statistics'/>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={total}
+        average={score/total}
+        positive={good*100/total}
+      />
+    </div>
+  )
+}
+
+export default App
